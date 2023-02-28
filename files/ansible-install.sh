2c4f8a52a74be1bc808129dedf9ff79f57c7eedf
Austin Calkins <austincalkins@gmail.com>
2023-02-27 22:17:50 -0700

    add autofs role

--- files/ansible-install.sh
+++ files/ansible-install.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install -y ansible
 sudo ansible-galaxy install gantsign.visual-studio-code
+sudo ansible-galaxy install robertdebock.autofs
